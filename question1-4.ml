ff66b247a685c19b32f36470370ad560cf33b1f2
acdale5774 <acdale5774@gmail.com>
2019-10-08 09:21:00 -0400

    fixed 1-4

--- question1-4.ml
+++ question1-4.ml
@@ -1,20 +1,33 @@
-(* Question 1.2: Given two strings, write a method to decide if 
- * one is a permutation of the other.
+(* Question 1.4: Given a string, write a function to check if it is a
+ * permutation of a palindrome.
  *)
 
 open String;;
+
+let rec size l =
+    match l with
+        [] -> 0
+        | h::t -> 1 + size t;;
+
+let rec minusLast l =
+    match l with
+        [] -> []
+        | [x] -> []
+        | h::t -> [h] @ minusLast t;;
 
 let rotate1 l =
     match l with
         [] -> []
         | h::t -> t @ [h];;
 
-let rec rotate l i k = 
+let rec rotateInner l i k = 
     match l with
         [] -> []
         | h::t -> if (i = k) then l
                     else
-                        rotate (rotate1 l) (i+1) k;;
+                        rotateInner (rotate1 l) (i+1) k;;
+
+let rotate l n = rotateInner l 0 n;;
 
 let explode s =
   let rec exp i l =
@@ -25,6 +38,11 @@ let implode c_l =
     let buf = Buffer.create 16 in
         List.iter (Buffer.add_char buf) c_l;
         Buffer.contents buf;;
+
+let rec getList ll =
+    match ll with
+        [] -> []
+        | h::t -> [h] @ getList t;;
 
 let rec getStringList c_ll =
     match c_ll with
@@ -41,4 +59,34 @@ let rec permInner c r orig =
                 permInner (c@[h]) (rotate1 t) t 
                     @ permInner c (rotate1 r) orig;;
 
-let perm s = getStringList (permInner [] (rotate1 (explode s)) (explode s));;
+let perm l = getList (permInner [] (rotate1 l) l);;
+
+let permStr s = getStringList (permInner [] (rotate1 (explode s)) (explode s));;
+
+let rec isPalindrome l =
+    match l with
+        [] -> true
+        | [x] -> true
+        | h::t -> match (rotate l ((size l) - 1)) with
+            [] -> false
+            | h2::t2 -> if h = h2
+                        then
+                            isPalindrome (minusLast t)
+                        else
+                            false;;
+
+let isPalindromeStr s = isPalindrome (explode s);;
+
+let rec containsPal l =
+    match l with
+        [] -> false
+        | h::t ->
+                if isPalindrome h
+                    then
+                        true
+                    else
+                        containsPal t;;
+
+let isPermOfPal l = containsPal (perm l);;
+
+let isPermOfPalStr s = isPermOfPal (explode s);;
